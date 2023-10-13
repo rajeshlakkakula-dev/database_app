f6f1c2f6fd12a4ca83a57c4abf4690c402922c7d
Rajesh Lakkakula <rajeshlakkakula536@gmail.com>
2023-10-13 16:25:11 +0530

    This commit contains the CRUD Operations complete code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 import 'package:database_app/Employees.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +5,6 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 void main() async {
-
   // Errors caused by Flutter Upgrade
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -20,14 +18,51 @@ void main() async {
      * */
       join(await getDatabasesPath(), 'emp_database.db'),
       onCreate: (db, version) {
-        return db.execute(
-          'CREATE TABLE employees(id INTEGER PRIMARY KEY,name TEXT , email TEXT)',
-        );
-      },
-    version: 1
-  );
+    return db.execute(
+      'CREATE TABLE employees(id INTEGER PRIMARY KEY,name TEXT , email TEXT)',
+    );
+  }, version: 1);
 
+  // Fetch/Retrieve Data
+  Future<List<Employees>> EmployeesList() async {
+    // Get the reference
+    final db = await database;
 
+    final List<Map<String, dynamic>> maps = await db.query('employees');
+
+    return List.generate(maps.length, (index) {
+      return Employees(
+          id: maps[index]['id'],
+          name: maps[index]['name'],
+          email: maps[index]['email']);
+    });
+  }
+
+  print(await EmployeesList());
+
+  //Update Employees Data
+  Future<void> updateEmployees(Employees employees) async {
+    final db = await database;
+
+    await db.update(
+      'employees',
+      employees.toMap(),
+      where: 'id =?',
+      whereArgs: [employees.id],
+    );
+  }
+
+  Future<void> deleteEmployees(int id) async {
+    final db = await database;
+
+    await db.delete(
+      'employees',
+      where: 'id =?',
+      whereArgs: [id],
+    );
+  }
+
+  // Insert Data
   Future<void> insertEmployee(Employees employees) async {
     // Get a reference to the database
     final db = await database;
@@ -39,6 +74,20 @@ void main() async {
 
   var venkatesh =
       Employees(id: 01, name: 'Venkatesh', email: 'venkatesh@gmail.com');
+  var nagaharish =
+      Employees(id: 02, name: 'nagaharish', email: 'nagaharish@gmail.com');
+  var manoj = Employees(id: 03, name: 'manoj', email: 'manoj@gmail.com');
+  var jitendra =
+      Employees(id: 04, name: 'jitendra', email: 'jitendra@gmail.com');
+  var ruchita = Employees(id: 05, name: 'ruchita', email: 'ruchita@gmail.com');
+
+  manoj = Employees(id: manoj.id + 8, name: manoj.name, email: manoj.email);
+
+  await updateEmployees(manoj);
 
   await insertEmployee(venkatesh);
+  await insertEmployee(nagaharish);
+  await insertEmployee(manoj);
+  await insertEmployee(jitendra);
+  await insertEmployee(ruchita);
 }
